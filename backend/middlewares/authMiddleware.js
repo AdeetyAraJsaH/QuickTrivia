85cc1ce1533314a3ffe55f80fc75c8cd19c44ae8
Aditya Raj Sah <adityash480@gmail.com>
2024-05-11 17:44:09 +0530

    refactor: send quiz data as a response

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,6 +1,7 @@
 import expressAsyncHandler from "express-async-handler"
 import jwt from 'jsonwebtoken'
 import User from "../models/userModels.js"
+import QuizData from "../models/quizModel.js"
 
 export const auth = expressAsyncHandler(async (req, res, next) => {
 
@@ -11,12 +12,18 @@ export const auth = expressAsyncHandler(async (req, res, next) => {
     if (token) {
         try {
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            console.log(decoded);
+            // console.log(decoded);
             req.user = await User.findById(decoded.userID).select('-password');
+            if (req.user.email) {
+                const email = req.user.email
+                // console.log(req.user.email);
+                req.quizData = await QuizData.find({ email }, { _id: 1, details: 1, createdAt: 1, Result: 1 });
+            }
             next();
         } catch (error) {
             res.status(401)
-            throw new Error("Not Authorized , Invalid Token !");
+            console.log(error);
+            // throw new Error("Not Authorized , Invalid Token !");
         }
     } else {
         res.status(401)
