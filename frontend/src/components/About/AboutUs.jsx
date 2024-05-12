f46aa3ae902f1d71971ca3c5ae355f830b9d8a24
Aditya Raj Sah <adityash480@gmail.com>
2024-05-12 21:06:23 +0530

    chore: adjust image size

--- frontend/src/components/About/AboutUs.jsx
+++ frontend/src/components/About/AboutUs.jsx
@@ -6,9 +6,8 @@ export default function About() {
         <div className="py-16 bg-white">
             <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
                 <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
-                    <div className="md:5/12 lg:w-5/12">
-                        <ImageComponent src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png"
-                            alt="image" />
+                    <div>
+                        <ImageComponent src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png" alt="image" />
                     </div>
                     <div className="md:7/12 lg:w-6/12">
                         <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
