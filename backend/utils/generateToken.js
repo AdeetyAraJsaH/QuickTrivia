9fa9910c522adec69ed0d6904221dc0579c11f74
Aditya Raj Sah <adityash480@gmail.com>
2024-06-16 23:22:07 +0530

    refactor: cookie config

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = (res, userID) => {
     res.cookie('jwt', token, {
         httpOnly: false,
         secure: true,
-        sameSite: 'strict',
+        sameSite: 'none',
         maxAge: 30 * 24 * 60 * 60 * 1000,
     })
 }
