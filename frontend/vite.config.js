856c04dead5fffe7f99ae45c0c994571a4dff88a
Aditya Raj Sah <adityash480@gmail.com>
2024-06-16 19:13:40 +0530

    modified:   frontend/vite.config.js change proxy target

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         // target: "http://localhost:5000",
-        target: "https://quicktrivia.onrender.com",
+        target: "https://quicktrivia.onrender.com/",
         changeOrigin: true
       },
     },
