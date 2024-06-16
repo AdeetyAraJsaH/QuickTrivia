fe0562039656fcd5e1ca5e5c1458dcff92aef7b5
Aditya Raj Sah <adityash480@gmail.com>
2024-06-16 19:48:10 +0530

    fix: cors

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,19 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 import dotenv from 'dotenv'
 dotenv.config()
 import userRoutes from './routes/userRoutes.js'
 import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middlewares/errorMiddleware.js'
 
+const app = express();
+app.use(cors());
+app.use(cors({origin:process.env.CLIENT_PORT}))
 connectDB();
 const port = process.env.PORT || 5000;
-const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use('/api/users', userRoutes);
 app.get('/', (req, res) => { res.send('Server is ready.') });
