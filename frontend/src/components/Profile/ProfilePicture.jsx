dac2fc7f5318630835beb4416d517086a0b1bc39
Aditya Raj Sah <adityash480@gmail.com>
2024-06-16 14:01:11 +0530

    feat: can change avatar

---
+++ frontend/src/components/Profile/ProfilePicture.jsx
@@ -0,0 +1,63 @@
+import { Avatar, Button } from '@nextui-org/react';
+import axios from 'axios'
+import React from 'react'
+
+function ProfilePicture({avatar}) {
+
+    let file = null;
+    const [fileName, setFileName] = React.useState('No file chosen, yet.');
+    const fileInputRef = React.useRef(null);
+
+    const handleButtonClick = () => {
+        fileInputRef.current.click();
+    };
+
+    const handleFileChange = (event) => {
+        file = event.target.files[0];
+        console.log(file)
+        if (file) {
+            setFileName(file.name);
+        } else {
+            setFileName('No file chosen, yet.');
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('profileImage',fileInputRef.current.files[0] );
+        await axios.post('/api/users/profile', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            },
+        })
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => {
+                console.error('Error uploading image:', err);
+            });
+    };
+
+    return (
+        <form onSubmit={handleSubmit} method='POST' encType='multipart/form-data' className='flex flex-col justify-center items-center gap-4'>
+            <Avatar src={avatar} className="sm:w-10 sm:h-10 md:w-24 md:h-24 text-large" />
+            {/* <img src={avatar} alt="uploaded img" /> */}
+            <div className='space-x-4'>
+                <input
+                    name='profileImage'
+                    type="file"
+                    accept='.jpg, .jpeg, .png'
+                    ref={fileInputRef}
+                    onChange={handleFileChange}
+                    style={{ display: 'none' }}
+                />
+                <Button onClick={handleButtonClick}>Choose a file</Button>
+                <span>{fileName}</span>
+            </div>
+            <Button className='text-white bg-orange-700 font-bold' type="submit">Upload</Button>
+        </form>
+    );
+}
+
+export default ProfilePicture
