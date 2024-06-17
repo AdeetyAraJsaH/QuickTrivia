3dbcffeb467d771d8324cd076adcd3ee1c819876
Aditya Raj Sah <adityash480@gmail.com>
2024-06-17 12:39:21 +0530

    refactor : cors code

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,8 @@ import { notFound, errorHandler } from './middlewares/errorMiddleware.js'
 const app = express();
 connectDB();
 app.use(cors());
-app.use(cors({origin:process.env.CLIENT_PORT}))
+app.set("trust proxy", 1);
+app.use(cors({ origin: process.env.CLIENT_PORT, credentials: true }))
 const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
